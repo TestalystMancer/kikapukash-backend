c979dc29c47cf00346920f17750e770c30a1e517
Daniel Tajiri <dannjue04@outlook.com>
2025-04-05 22:17:38 +0300

    Add SavingsGroupViewSet and serializers for SavingsGroup and SavingsGroupMember

---
+++ savings_group/serializers/SavingsGroupSerializer.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from savings_group.models import SavingsGroup,SavingsGroupMember
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+class SavingsGroupMemberSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=get_user_model().objects.all())  
+    is_admin = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = SavingsGroupMember
+        fields = ['user', 'is_admin']
+
+    def validate_user(self, value):
+        if SavingsGroupMember.objects.filter(savings_group=self.instance.savings_group, user=value).exists():
+            raise serializers.ValidationError("This user is already a member of the group.")
+        return value
+
+    def create(self, validated_data):
+        return SavingsGroupMember.objects.create(**validated_data)
+
+class SavingsGroupSerializer(serializers.ModelSerializer):
+    members = SavingsGroupMemberSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SavingsGroup
+        fields = '__all__'
+    
+    def validate_group_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Group name must be at least 3 characters long.")
+        return value
--- savings_group/views.py
+++ savings_group/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from savings_group.models import SavingsGroup
+from .serializers import SavingsGroupSerializer
 
-# Create your views here.
+class SavingsGroupViewSet(viewsets.ModelViewSet):
+    queryset = SavingsGroup.objects.all()  # Retrieve all savings groups
+    serializer_class = SavingsGroupSerializer  # Use the serializer defined earlier
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access the viewset
